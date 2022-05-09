15eda7e71b6b27979eeb3d74397c8536b0fa1679
luv28 <70091184+luv28@users.noreply.github.com>
2022-05-10 02:53:04 +0530

    Time: 4 ms (26.35%), Space: 6.5 MB (84.81%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     vector<string> m={"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"},ans;
-    void helper(string &digits,int ci,string s){
+    void helper(string &digits,int ci,string s){  //for backtracking use string &digits
         if(ci==digits.length()){
             ans.push_back(s);
             return;
-        }
-        for(auto a:m[digits[ci]-'2']) helper(digits,ci+1,s+a);
-    }
+        }                                                                  //for backtracking
+        for(auto a:m[digits[ci]-'2']) helper(digits,ci+1,s+a);       //s.push_back(a); helper(digits,ci+1,s);
+    }                                                                   //s.pop_back();
     vector<string> letterCombinations(string digits) {
         if(digits.length()==0) return ans;
         helper(digits,0,"");
@@ -16,15 +16,3 @@ public:
     
 };
 
-// vector<string> m = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"}, ans;    
-// vector<string> letterCombinations(string digits) {
-// 	if(digits == "") return ans;         
-// 	helper(digits, 0, "");              
-// 	return ans;
-// }
-
-// void helper(string &digits, int i, string s){	
-// 	if(i == digits.length()) ans.push_back(s);    
-// 	else 
-// 		for(auto c : mappings[digits[i] - '2']) helper(digits, i + 1, s+c);
-// }
