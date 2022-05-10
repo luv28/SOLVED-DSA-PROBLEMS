a5885a1ead8920e5d659bf41f2749a9857798043
luv28 <70091184+luv28@users.noreply.github.com>
2022-05-11 00:49:33 +0530

    Time: 11 ms (5.36%), Space: 13.7 MB (5.40%) - LeetHub

---
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    vector<vector<int>> v;
+public:
+    void helper(int k,int n,int num,vector<int> ve){
+        if(k==0){
+            if(n==0) v.push_back(ve);
+            return;
+        }
+        if(num>9) return;
+        ve.push_back(num);
+        helper(k-1,n-num,num+1,ve);
+        ve.pop_back();
+        helper(k,n,num+1,ve);
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<int> ve;
+        helper(k,n,1,ve);
+        return v;
+    }
+};
