2dd16f3d700a829e15769e5c45a8e8dac4bb7f00
luv28 <70091184+luv28@users.noreply.github.com>
2022-05-19 13:50:30 +0530

    Time: 49 ms (82.19%), Space: 14.9 MB (95.23%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -1,7 +1,8 @@
 class Solution {
     int dp[201][201];
+    vector<vector<int>> dir={{1,0},{-1,0},{0,1},{0,-1}};
 public:
-    int helper(int r,int c,int m,int n, vector<vector<int>>& grid,vector<vector<int>> &dir){
+    int helper(int r,int c,int m,int n, vector<vector<int>>& grid){
         // if(r<0 || r>=m || c<0 || c>=n) return 0;
         if(dp[r][c]!=-1) return dp[r][c];
         int ans=0;
@@ -10,7 +11,7 @@ public:
             int nc=c+dir[i][1];
             int temp=0;
             if(nr>=0 && nr<m && nc>=0 && nc<n && grid[nr][nc]>grid[r][c]) 
-                temp=helper(nr,nc,m,n,grid,dir)+1;
+                temp=helper(nr,nc,m,n,grid)+1;
             ans=max(ans,temp);
         }
         return dp[r][c]=ans;
@@ -20,12 +21,11 @@ public:
         int m=matrix.size();
         int n=matrix[0].size();
         if(m==1 && n==1) return 1;
-        vector<vector<int>> dir={{1,0},{-1,0},{0,1},{0,-1}};
         memset(dp,-1,sizeof(dp));
         int ans=INT_MIN;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                int temp=1+helper(i,j,m,n,matrix,dir);
+                int temp=1+helper(i,j,m,n,matrix);
                 ans=max(ans,temp);
             }
         }
