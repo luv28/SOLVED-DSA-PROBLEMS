b0e28fb0ebcc0e30812a4556bc08caf7bc3dea50
luv28 <70091184+luv28@users.noreply.github.com>
2022-05-20 15:55:29 +0530

    Time: 4 ms (62.19%), Space: 7.6 MB (89.47%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,20 +1,22 @@
 class Solution {
+    int map[101][101];
 public:
-    int helper(int r,int c,int m,int n,unordered_map<string,int> &map,vector<vector<int>>& obstacleGrid){
+    int helper(int r,int c,int m,int n,vector<vector<int>>& obstacleGrid){
         if(r==m-1 &&c==n-1) return 1;
         if(r>m-1 || c>n-1 || obstacleGrid[r][c]==1) return 0;
-        string s=to_string(r)+"-"+to_string(c);
-        if(map.find(s)!=map.end()) return map[s];
-        int right=helper(r,c+1,m,n,map,obstacleGrid);
-        int down=helper(r+1,c,m,n,map,obstacleGrid);
-        return map[s]=right+down;
+        // string s=to_string(r)+"-"+to_string(c);
+        // if(map.find(s)!=map.end()) return map[s];
+        if(map[r][c]!=-1) return map[r][c];
+        int right=helper(r,c+1,m,n,obstacleGrid);
+        int down=helper(r+1,c,m,n,obstacleGrid);
+        return map[r][c]=right+down;
     }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        unordered_map<string,int> map;
+        // unordered_map<string,int> map;
         int m=obstacleGrid.size();
         int n=obstacleGrid[0].size();
         if(obstacleGrid[m-1][n-1]==1) return 0;
-        int ans=helper(0,0,m,n,map,obstacleGrid);
-        return ans;
+        memset(map,-1,sizeof(map));
+        return helper(0,0,m,n,obstacleGrid);
     }
 };
