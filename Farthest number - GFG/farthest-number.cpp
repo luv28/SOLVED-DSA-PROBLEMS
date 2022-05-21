37f26bf79560eba401a42c11ccf528e5d51c06ef
luv28 <70091184+luv28@users.noreply.github.com>
2022-05-21 22:21:33 +0530

    Added solution - LeetHub

---
+++ Farthest number - GFG/farthest-number.cpp
@@ -0,0 +1,44 @@
+// { Driver Code Starts
+//Initial Template for C++
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+class Solution{   
+  public:
+    vector<int> farNumber(int n,vector<int> arr){
+        vector<int> v;
+        for(int i=0;i<n;i++){
+            int ans=-1;
+            for(int j=n-1;j>=i;j--){
+                if(arr[i]>arr[j]){
+                    v.push_back(j);
+                    break;
+                }
+                if(i==j) v.push_back(-1);
+            }
+        }
+        return v;
+    }
+};
+
+// { Driver Code Starts.
+signed main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        vector<int> Arr(N);
+        for(int i=0;i<N;i++)
+            cin>>Arr[i];
+        Solution obj;
+        vector<int> answer=obj.farNumber(N,Arr);
+        for(auto i:answer)
+            cout<<i<<" ";
+        cout<<endl;
+  }
+}  // } Driver Code Ends
