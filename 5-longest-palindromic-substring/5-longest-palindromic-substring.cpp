74e3689fbe9196025c92e431db35f19bbd7a6939
luv28 <70091184+luv28@users.noreply.github.com>
2022-05-23 00:03:51 +0530

    Time: 26 ms (83.19%), Space: 7.1 MB (92.29%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -7,19 +7,30 @@ public:
         return true;
     }
     string longestPalindrome(string s) {
+        int n=s.size();
         int maxx=0;
         pair<int,int> p;
-        int n=s.size();
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(ispalindrome(s,i,j)){
-                    if(maxx<j-i+1){
-                        maxx=j-i+1;
-                        p={i,j};
-                    }
+        for(int i=1,l=0,r=2;i<n;i++,l=i-1,r=i+1){
+            while(l>=0 && r<n && s[l]==s[r]){
+                if(maxx<r-l+1){
+                    maxx=r-l+1;
+                    p={l,r};
                 }
+                l--;
+                r++;
             }
         }
-        return s.substr(p.first,p.second-p.first+1);;
+        for(int i=0,l=0,r=1;i<n-1;i++,l=i,r=i+1){
+            while(l>=0 && r<n && s[l]==s[r]){
+                if(maxx<r-l+1){
+                    maxx=r-l+1;
+                    p={l,r};
+                }
+                l--;
+                r++;
+            }
+        }
+        return s.substr(p.first,p.second-p.first+1);
     }
 };
+
