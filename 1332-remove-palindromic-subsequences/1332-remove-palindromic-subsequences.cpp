cdc4d4058fa393a577158c3a89463c45cde57285
Luv Khandelwal <70091184+luv28@users.noreply.github.com>
2022-06-08 16:02:27 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (26.79%) - LeetHub

---
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        int l=0,r=s.size()-1;
+        while(l<r){
+            if(s[l]==s[r]){
+                l++;
+                r--;
+            }
+            else return 2;
+        }
+        return 1;
+    }
+};
