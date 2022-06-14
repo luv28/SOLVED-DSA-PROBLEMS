ffb86be30d9cd151dbced6ff9a337230234a06cb
Luv Khandelwal <70091184+luv28@users.noreply.github.com>
2022-06-14 17:27:44 +0530

    Time: 4 ms (49.19%), Space: 6.6 MB (23.95%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
@@ -4,16 +4,15 @@ public:
         unordered_map<char,int> m;
         if(s.length()<3) return 0;
         int i=0,count=0,j=0,n=s.length();
-        while(j<3){
-            m[s[j++]]++;
-        }
-        if(m.size()==3) count++;
-        for(int i=3;i<n;i++){
-            m[s[i]]++;
-            m[s[i-3]]--;
-            if(m[s[i-3]]==0) m.erase(s[i-3]);
-            if(m.size()==3) count++;
-
+        while(j<n){
+            m[s[j]]++;
+            if(j-i+1==3){
+                if(m.size()==3) count++;
+                m[s[i]]--;
+                if(m[s[i]]==0) m.erase(s[i]);
+                i++;
+            }
+            j++;
         }
         return count;
     }
