5fe23a06eea7c837ca9e6584a9fdf93945ddd01e
Luv Khandelwal <70091184+luv28@users.noreply.github.com>
2022-06-19 17:12:00 +0530

    Time: 95 ms (10.72%), Space: 18.2 MB (14.45%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,13 +1,27 @@
 class Solution {
 public:
+    
+    int dr[4]={-1,1,0,0};
+    int dc[4]={0,0,-1,+1};
+    bool isValid(int r,int c,int n,int m){
+        return r>=0 && c>=0 && r<n && c<m;
+    }
     void helper(vector<vector<char>> &grid, int r,int c,int n,int m){
-        if(r<0 || c<0 || r>=n || c>=m || grid[r][c]=='0') return;
-        grid[r][c]='0';
-        helper(grid,r-1,c,n,m);
-        helper(grid,r+1,c,n,m);
-        helper(grid,r,c-1,n,m);
-        helper(grid,r,c+1,n,m);
-
+        queue<pair<int,int>> q;
+        q.push({r,c});
+        while(!q.empty()){
+            int row=q.front().first;
+            int col=q.front().second;
+            q.pop();
+            for(int i=0;i<4;i++){
+                int nr=row+dr[i];
+                int nc=col+dc[i];
+                if(isValid(nr,nc,n,m)&&grid[nr][nc]=='1'){
+                    q.push({nr,nc});
+                    grid[nr][nc]='0';
+                } 
+            }    
+        }
     }
     int numIslands(vector<vector<char>>& grid) {
         int islands=0;
