a89cddc604911f6cdb1758a5cf39536a0fbe746c
Luv Khandelwal <70091184+luv28@users.noreply.github.com>
2022-06-20 17:07:21 +0530

    Time: 16 ms (98.10%), Space: 13.5 MB (75.42%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> indegree(numCourses);
+        vector<vector<int>> adjL(numCourses);
+        int visitedCourses=0;
+        vector<bool> vis(numCourses,false);
+        queue<int> q;
+        int n=prerequisites.size();
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            indegree[prerequisites[i][1]]++;
+            adjL[prerequisites[i][0]].push_back(prerequisites[i][1]);
+        }
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0) q.push(i);
+        }
+        while(!q.empty()){
+            int curr=q.front();
+            q.pop();
+            if(vis[curr]) continue;
+            vis[curr]=true;
+            visitedCourses++;
+            ans.push_back(curr);
+            for(auto a:adjL[curr]){
+                indegree[a]--;
+                if(indegree[a]==0) q.push(a);
+            }
+        }
+        if(visitedCourses==numCourses) return true;
+        else return false;
+    }
+};
