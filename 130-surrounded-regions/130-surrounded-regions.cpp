ce3d094e5558dc528ed88c7008c756837cf045a3
Luv Khandelwal <70091184+luv28@users.noreply.github.com>
2022-06-21 23:40:50 +0530

    Time: 29 ms (18.70%), Space: 10 MB (77.06%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.cpp
+++ 130-surrounded-regions/130-surrounded-regions.cpp
@@ -1,38 +1,28 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>> &board,int row,int col,char change){
+    void dfs(vector<vector<char>> &board,int row,int col){
         if(row<0 || row>=board.size() || col<0 || col>=board[0].size() || board[row][col]!='O') return;
-        board[row][col]=change;
-        dfs(board,row-1,col,change);
-        dfs(board,row+1,col,change);
-        dfs(board,row,col-1,change);
-        dfs(board,row,col+1,change);
-        return;
-    }
-    void dfsAgain(vector<vector<char>> &board,int row,int col,char detect,char change){
-        if(row<0 || row>=board.size() || col<0 || col>=board[0].size() || board[row][col]!=detect) return;
-        board[row][col]=change;
-        dfsAgain(board,row-1,col,detect,change);
-        dfsAgain(board,row+1,col,detect,change);
-        dfsAgain(board,row,col-1,detect,change);
-        dfsAgain(board,row,col+1,detect,change);
+        board[row][col]='R';
+        dfs(board,row-1,col);
+        dfs(board,row+1,col);
+        dfs(board,row,col-1);
+        dfs(board,row,col+1);
         return;
     }
     void solve(vector<vector<char>>& board) {
         int n=board.size(),m=board[0].size();
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if((i==0 || j==0 || i==n-1 || j==m-1) && board[i][j]=='O') dfs(board,i,j,'Z');
-            }
+            dfs(board,i,0);
+            dfs(board,i,m-1);
         }
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;j++){
-                if(board[i][j]=='O') dfs(board,i,j,'X');
-            }
+        for(int j=0;j<m;j++){
+            dfs(board,0,j);
+            dfs(board,n-1,j);
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if((i==0 || j==0 || i==n-1 || j==m-1) && board[i][j]=='Z') dfsAgain(board,i,j,'Z','O');
+                if(board[i][j]=='O') board[i][j]='X';
+                if(board[i][j]=='R') board[i][j]='O'; 
             }
         }
     }
