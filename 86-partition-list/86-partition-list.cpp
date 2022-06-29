12e6f47ded92ba1ceae37f1e200142027d60fa1a
Luv Khandelwal <70091184+luv28@users.noreply.github.com>
2022-06-30 01:51:15 +0530

    Time: 17 ms (7.61%), Space: 10.4 MB (31.99%) - LeetHub

---
+++ 86-partition-list/86-partition-list.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int B) {
+        if(!head || !(head->next)) return head;
+    ListNode *headB=nullptr,*headA=nullptr,*p=nullptr,*q=nullptr,*curr=head;
+    while(curr){
+        if(headA && headB) break;
+        if(curr->val<B && !headB) headB=curr;
+        else if(curr->val>=B && !headA) headA=curr;
+        curr=curr->next;
+    }
+    if(!headA || !headB) return head;
+    p=headB;
+    curr=head;
+    q=headA;
+    while(curr){
+        if(curr->val<B){
+            if(curr==p){
+                curr=curr->next;
+                continue;
+            }else{
+                p->next=curr;
+                p=p->next;
+            }
+        }else{
+            if(curr==q){
+                curr=curr->next;
+                continue;
+            }else{
+                q->next=curr;
+                q=q->next;
+            }
+        }
+        curr=curr->next;
+    }
+    p->next=headA;
+    q->next=nullptr;
+    return headB;
+    }
+};
