e86348fa798d7ec4c7ada81f7e0803be34a0d8f3
Luv Khandelwal <70091184+luv28@users.noreply.github.com>
2022-07-22 22:48:01 +0530

    Time: 9 ms (50.05%), Space: 10.4 MB (32.53%) - LeetHub

--- 86-partition-list/86-partition-list.cpp
+++ 86-partition-list/86-partition-list.cpp
@@ -11,40 +11,62 @@
 class Solution {
 public:
     ListNode* partition(ListNode* head, int B) {
-        if(!head || !(head->next)) return head;
-    ListNode *headB=nullptr,*headA=nullptr,*p=nullptr,*q=nullptr,*curr=head;
-    while(curr){
-        if(headA && headB) break;
-        if(curr->val<B && !headB) headB=curr;
-        else if(curr->val>=B && !headA) headA=curr;
-        curr=curr->next;
-    }
-    if(!headA || !headB) return head;
-    p=headB;
-    curr=head;
-    q=headA;
-    while(curr){
-        if(curr->val<B){
-            if(curr==p){
-                curr=curr->next;
-                continue;
-            }else{
-                p->next=curr;
-                p=p->next;
+        ListNode *less=new ListNode();        
+        ListNode *more=new ListNode();
+        ListNode *moreHead=more;
+        ListNode *curr=head;
+        head=less;
+        while(curr){
+            if(curr->val < B){
+                less->next=curr;
+                less=less->next;
             }
-        }else{
-            if(curr==q){
-                curr=curr->next;
-                continue;
-            }else{
-                q->next=curr;
-                q=q->next;
+            else{
+                more->next=curr;
+                more=more->next;
             }
+            curr=curr->next;
         }
-        curr=curr->next;
-    }
-    p->next=headA;
-    q->next=nullptr;
-    return headB;
+        more->next=nullptr;
+        less->next=moreHead->next;
+        return head->next;
     }
 };
+    
+    
+//     if(!head || !(head->next)) return head;
+//     ListNode *headB=nullptr,*headA=nullptr,*p=nullptr,*q=nullptr,*curr=head;
+//     while(curr){
+//         if(headA && headB) break;
+//         if(curr->val<B && !headB) headB=curr;
+//         else if(curr->val>=B && !headA) headA=curr;
+//         curr=curr->next;
+//     }
+//     if(!headA || !headB) return head;
+//     p=headB;
+//     curr=head;
+//     q=headA;
+//     while(curr){
+//         if(curr->val<B){
+//             if(curr==p){
+//                 curr=curr->next;
+//                 continue;
+//             }else{
+//                 p->next=curr;
+//                 p=p->next;
+//             }
+//         }else{
+//             if(curr==q){
+//                 curr=curr->next;
+//                 continue;
+//             }else{
+//                 q->next=curr;
+//                 q=q->next;
+//             }
+//         }
+//         curr=curr->next;
+//     }
+//     p->next=headA;
+//     q->next=nullptr;
+//     return headB;
+//     }
