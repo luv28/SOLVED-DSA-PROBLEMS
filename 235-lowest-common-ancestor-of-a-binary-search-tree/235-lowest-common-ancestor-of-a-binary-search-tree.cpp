0d76ead5303f2e43293f2c78089996353e66b765
Luv Khandelwal <70091184+luv28@users.noreply.github.com>
2022-07-27 01:24:47 +0530

    Time: 61 ms (16.48%), Space: 23.3 MB (63.67%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -14,8 +14,10 @@ public:
         if(root==nullptr || root==p || root==q) return root;
         TreeNode *l=lowestCommonAncestor(root->left,p,q);
         TreeNode *r=lowestCommonAncestor(root->right,p,q);
-        if(l==nullptr) return r;
-        else if(r==nullptr) return l;
+        if(!l) return r;
+        else if(!r) return l;
         else return root;
     }
 };
+
+        
